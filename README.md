11d0aa94efdc368a77e514061b84166864578b65
jaywaa <jamesbellairs@gmail.com>
2021-06-01 14:24:54 +0200

    Amendments to readme

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 A lambda that retrieves a token, and uses it to create a Stitch [PaymentInitiationRequest](https://stitch.money/docs/stitch-api/payment-requests) and returns the URL and ID
 
-## How to:
- * Clone the repository
- * Inject 
+## Getting started
+
+### 1. Set up environment
+Take a look at `function/settings.ts` to see how the environment variables are loaded.
+In order to run the Lambda, the following environment variables will need to be present: 
+
+* `STITCH_PRIVATE_KEY` -- the private key portion of the certificate issued to you by Stitch.
+* `STITCH_CLIENT_ID` -- the client ID issued to you by Stitch.
+
+This will need to be done locally, and on the environment the lambda will be running, see: [Configuring environment variables for AWS Lambdas](https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html).
+If you would rather set these values using a different method, you will need to configure that behaviour in `function/settings.ts`.
+
+### 2. Running locally
+Install: `npm run install`
+
+Run: `npm run start`
--- function/domain/settings.ts
+++ function/domain/settings.ts
@@ -3,26 +3,23 @@ import { buildStitchCertificate } from '../stitch/stitch-public-certificate';
 
 type Settings = {
     stitchClientId: string
-    stitchClientCertificate: string,
-    bankAccount: { accountNumber: string, bank: string }
+    stitchClientCertificate: string
 }
 
 export function getSettings(): Settings {
-    // Importing private key from environment variables, but could read this from disk too, or inject from CI pipeline
-    // Keep it secret and don't check this into your repository.
+    // Importing private key from environment variables, but could read this from disk too.
+    // Keep it secret. It should not be checked into your repository.
     let { STITCH_PRIVATE_KEY } = process.env;
-    const { STITCH_CLIENT_ID, BANK_ACCOUNT_JSON } = process.env;
+    const { STITCH_CLIENT_ID } = process.env;
 
     assert(STITCH_CLIENT_ID, 'Expected STITCH_CLIENT_ID to be defined in order to generate a client assertion.');
     assert(STITCH_PRIVATE_KEY, 'Expected STITCH_PRIVATE_KEY to be defined in order to generate a client assertion.');
-    assert(BANK_ACCOUNT_JSON, 'Expected BANK_ACCOUNT_JSON to be defined in order to generate a payment request.');
 
-    const { accountNumber, bank } = JSON.parse(BANK_ACCOUNT_JSON);
+    // have used '\\n' characters as line breaks in the environment variable
     STITCH_PRIVATE_KEY = STITCH_PRIVATE_KEY.split('\\n').join('\n');
 
     return {
         stitchClientId: STITCH_CLIENT_ID,
         stitchClientCertificate: buildStitchCertificate(STITCH_PRIVATE_KEY),
-        bankAccount: { accountNumber, bank }
     }
 }
